337e56b429d7d63d1de890f1f85f8eac77bdbea1
RoshanShrestha07 <roshanstan787@gmail.com>
2025-07-10 20:26:39 +0545

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,31 @@
+print("hello world")
+# MOdules
+# A module is a file containing code written by somebody else which can be imported and used in our programs
+
+
+poem = """Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are"""
+
+print(poem)
+# a=10
+# b=5
+# a=b
+# print(a)
+# print(11%4)
+# print(type (11/4))
+base= 5
+for i in range(1,11):
+    result= base * 1
+    print(f"{base}x {i} = {result}")
+
+
+    age=25
+    print("Age is age")
+    print(F"Age is {age}")
+
+
+
